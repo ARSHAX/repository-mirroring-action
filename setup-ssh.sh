ac2ca6622de89fa4e9b68d156fc739e9a8cb5f2b
shahnikba <nikbakhtian@gmail.com>
2021-11-23 23:59:29 +0000

    Update setup-ssh.sh
    Add the config file needed for AWS

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -4,3 +4,5 @@ set -eu
 mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+echo "$INPUT_SSH_CONFIG" > ~/.ssh/config && chmod 600 ~/.ssh/config
+echo "$INPUT_HOST_KEY" > known_hosts && chmod 600 known_hosts && cd ..
