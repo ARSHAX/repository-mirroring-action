ae50f1c0e81807d97e588df1d1d33a0a086c4086
shahnikba <nikbakhtian@gmail.com>
2021-11-24 22:51:50 +0000

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -6,5 +6,7 @@ set -eu
 #export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 #git remote add mirror "$INPUT_TARGET_REPO_URL"
 #git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
-git clone ssh://"$INPUT_HOST_KEY"@git-codecommit.ap-east-1.amazonaws.com/v1/repos/data_recorder
+#git clone ssh://"$INPUT_HOST_KEY"@git-codecommit.ap-east-1.amazonaws.com/v1/repos/data_recorder
+git remote add mirror ssh://"$INPUT_HOST_KEY"@git-codecommit.ap-east-1.amazonaws.com/v1/repos/data_recorder
+git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
