057373553a513080d083b94bacd95244d18e743e
shahnikba <nikbakhtian@gmail.com>
2021-11-24 22:46:29 +0000

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 set -eu
 
-mkdir -p ~/.ssh
-echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo "$INPUT_SSH_CONFIG" > ~/.ssh/config && chmod 600 ~/.ssh/config
-echo "$INPUT_HOST_KEY" >  ~/.ssh/known_hosts && chmod 600  ~/.ssh/known_hosts
+mkdir -p /root/.ssh/
+
+echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
+echo "$INPUT_SSH_PUBLIC_KEY" > /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa.pub
+
+
+echo "$INPUT_SSH_CONFIG" > /root/.ssh/config && chmod 600 /root/.ssh/config
+
+touch /root/.ssh/known_hosts
+ssh-keyscan git-codecommit.ap-east-1.amazonaws.com >> /root/.ssh/known_hosts
+
+
