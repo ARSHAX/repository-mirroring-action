4a3ab667a45a68bc3dbf7477f74984a9524f9397
shahnikba <nikbakhtian@gmail.com>
2021-11-24 00:23:47 +0000

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo "$INPUT_SSH_CONFIG" > ~/.ssh/config && chmod 600 ~/.ssh/config
-echo "$INPUT_HOST_KEY" > known_hosts && chmod 600 known_hosts && cd ..
+echo "$INPUT_HOST_KEY" >  ~/.ssh/known_hosts && chmod 600  ~/.ssh/known_hosts
